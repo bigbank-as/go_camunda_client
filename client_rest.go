5ed3d750fc63534cf7a53b9ba1709b3c51d36a72
Valdas Petrulis <petrulis.valdas@gmail.com>
2016-10-15 21:09:43 +0300

    Do requests thru Client

--- client_rest.go
+++ client_rest.go
@@ -3,6 +3,7 @@ package camunda_client
 import (
 	"encoding/json"
 	"github.com/bigbank/camunda_client/dto"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -10,42 +11,53 @@ import (
 func Construct(urlRoot string) CamundaClient {
 	client := new(camundaClientRest)
 	client.urlRoot = urlRoot
+	client.httpClient = http.Client{}
 
 	return client
 }
 
-func (client *camundaClientRest) GetProcess(processId string) dto.Process {
+func (client camundaClientRest) GetProcess(processId string) dto.Process {
 	var process dto.Process
 
-	jsonData, err := fetchGet(client.urlRoot + "/process-instance/" + processId)
+	response, err := client.doRequest("GET", client.urlRoot + "/process-instance/" + processId, nil)
 	if err == nil {
-		return process
+		client.parseResponseJson(response, &process)
 	}
-
-	json.Unmarshal(jsonData, &process)
 
 	return process
 }
 
 type camundaClientRest struct {
-	urlRoot string
+	urlRoot    string
+	httpClient http.Client
 }
 
-func fetchGet(url string) ([]byte, error) {
-	response, err := http.Get(url)
+func (client camundaClientRest) doRequest(method, url string, payload io.Reader) (*http.Response, error) {
+	request, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		return nil, err
 	}
+
+	response, err := client.httpClient.Do(request)
+	if err != nil {
+		return response, err
+	}
+
 	defer response.Body.Close()
 
-	return readResponse(response)
+	return response, nil
 }
 
-func readResponse(response *http.Response) ([]byte, error) {
-	bytes, err := ioutil.ReadAll(response.Body)
+func (client camundaClientRest) parseResponseJson(response *http.Response, dto interface{}) error {
+	responseJson, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return bytes, nil
+	err = json.Unmarshal(responseJson, dto)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
